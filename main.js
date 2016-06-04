46280cf36ef93a2d9ff3d0ffce5099df0021bbe6
Keiichiro Ono <keiono@gmail.com>
2016-06-03 18:03:46 -0700

    Locaiton of files moved.

--- main.js
+++ main.js
@@ -1,64 +1,78 @@
-const electron = require('electron');
-const WebSocket = require('ws');
+const {app, globalShortcut, BrowserWindow} = require('electron')
+const WebSocket = require('ws')
 
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const WS_ADDRESS = "ws://localhost:8025/ws/echo"
+
+// Local connection to Cytoscape app
+let ws
+
 let mainWindow
 
-let ws = new WebSocket("ws://localhost:8025/ws/echo")
-
 function createWindow () {
+
+  // Establish WS connection
+  try {
+    // Try Connection to server...
+    ws = new WebSocket(WS_ADDRESS)
+
+    ws.onopen = function() {
+      app.on('browser-window-focus', function() {
+        var msg = {
+          from: "ndex",
+          type: "focus",
+          body: "Ndex focused "
+        };
+
+        ws.send(JSON.stringify(msg));
+      });
+    };
+
+    //Listen for messages
+    ws.onmessage = function(event) {
+      var msg = JSON.parse(event.data)
+      switch(msg.type) {
+        case "focus":
+          //Bring NDEx Valet into focus
+          if(msg.from === "cy3") {
+            mainWindow.show();
+          }
+          break;
+      }
+    };
+
+    ws.onclose = function() {
+      app.quit()
+    };
+
+  } catch (e) {
+    console.log(e);
+  }
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 750})
+  mainWindow = new BrowserWindow({width: 1000, height: 780})
+  mainWindow.loadURL(`file://${__dirname}/ndex/index.html`)
+  mainWindow.webContents.openDevTools();
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 
-  // Setup WS
-
-  ws.onopen = function() {
-    app.on('browser-window-focus', function() {
-      var msg = {
-        from: "ndex",
-        type: "focus",
-        body: "Ndex focused "
-      };
-
-      ws.send(JSON.stringify(msg));
-    });
-  };
-
-  //Listen for messages
-  ws.onmessage = function(event) {
-    var msg = JSON.parse(event.data)
-    switch(msg.type) {
-      case "focus":
-        //Bring NDEx Valet into focus
-        if(msg.from === "cy3") {
-          mainWindow.show();
-        }
-        break;
-    }
-  }
 }
 
-app.on('ready', createWindow)
+function addShortcuts() {
+  globalShortcut.register('CommandOrControl+w', function() {
+    console.log('Close (w) is pressed');
+    app.quit()
+  });
+}
+
+app.on('ready', () => {
+  createWindow()
+  addShortcuts()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -66,8 +80,6 @@ app.on('window-all-closed', function () {
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
